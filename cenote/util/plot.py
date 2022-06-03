191589f882c51005b43e8cd02e63f02a097ea7cd
Robert Cofield <ozymandium@protonmail.com>
2022-06-02 20:45:53 -0500

    horizontal cursor as well

--- cenote/util/plot.py
+++ cenote/util/plot.py
@@ -5,39 +5,76 @@ from cenote import gas_usage as gu
 from cenote import config
 
 
-# class Synchronizer:
+class Synchronizer:
 
-#     def __init__(self):
+    def __init__(self):
+        self.callbacks = []
+
+    def register(self, callback):
+        self.callbacks.append(callback)
+
+    def time_update(self, time):
+        for callback in self.callbacks:
+            callback(time)
+
 
 class ProfilePlot:
 
-    def __init__(self, plan: gu.Plan, result: gu.Result):
+    def __init__(self, plan: gu.Plan, result: gu.Result, sync: Synchronizer):
+        # internal state
+        self.times = plan.times()
+        self.depths = plan.depths()
+        self.time = 0
 
+        # generate plot entities
         self.fig, self.ax = plt.subplots()
-
-        self.time = plan.times()
-        self.depth = plan.depths()
-
-        self.cursor = self.ax.plot([0, 0], [np.min(self.depth), np.max(self.depth)], "m", alpha=0.7)[0]
-
-        self.ax.plot(self.time, self.depth, "g")
+        self.x_cursor = self.ax.plot(
+            [np.min(self.times), np.min(self.times)], 
+            [np.min(self.depths), np.max(self.depths)], 
+            "m", alpha=0.5)[0]
+        self.y_cursor = self.ax.plot(
+            [np.min(self.times), np.max(self.times)], 
+            [np.min(self.depths), np.min(self.depths)],
+            "m", alpha=0.5)[0]
+        self.ax.plot(self.times, self.depths, "g")
         
+        # plot accoutrements
         self.ax.set_title("Depth Profile")
-        self.ax.grid(alpha=0.3)
+        self.ax.grid(alpha=0.2)
         self.ax.set_xlabel("Time ({})".format(str(config.TIME_UNIT)))
         self.ax.set_ylabel("Depth ({})".format(str(config.DEPTH_UNIT)))
         self.ax.invert_yaxis()
 
+        # callback setup
         self.fig.canvas.mpl_connect('button_press_event', self.mouse_handler)
+        self.fig.canvas.mpl_connect("key_press_event", self.key_handler)
+
+        # synchronizer setup
+        self.sync = sync
+        self.sync.register(self.time_update_callback)
 
     def mouse_handler(self, event):
         if event.xdata is None:
             return
-        self.update_cursor(event.xdata)
+        time = np.round(event.xdata, 1) # 6 second increments
+        self.sync.time_update(time)
 
-    def update_cursor(self, time):
-        self.cursor.set_xdata([event.xdata, event.xdata])
-        self.ax.draw_artist(self.cursor)
+    def key_handler(self, event):
+        pass
+
+    def time_update_callback(self, time):
+        self.time = time
+        depth = np.interp(time, self.times, self.depths)
+
+        # x_cursor
+        self.x_cursor.set_xdata([self.time, self.time])
+        self.ax.draw_artist(self.x_cursor)
+
+        # y_cursor
+        self.y_cursor.set_ydata([depth, depth])
+        self.ax.draw_artist(self.y_cursor)
+
+        # update
         self.fig.canvas.draw()
         self.fig.canvas.blit(self.ax.bbox)
 
@@ -61,6 +98,8 @@ class ProfilePlot:
 
 def plot(plan: gu.Plan, result: gu.Result):
     plt.style.use('dark_background')
-    profile = ProfilePlot(plan, result)
+
+    sync = Synchronizer()
+    profile = ProfilePlot(plan, result, sync)
     # plot_gas_usage(plan, result)
     plt.show()
