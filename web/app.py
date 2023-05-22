81bfca4663e7f256021530ce7debdadf55ea4534
Robert Cofield <ozymandium@protonmail.com>
2023-05-22 12:06:03 -0500

    parsing finished but plotting the plan crashes unexpectedly

--- web/app.py
+++ web/app.py
@@ -4,6 +4,7 @@ import os
 import traceback
 import json
 import base64
+import pprint
 
 import flask
 import flask_wtf
@@ -51,40 +52,6 @@ def json_from_base64(blob: str) -> str:
     return base64.b64decode(blob)
 
 
-class State:
-    """No bungee/cenote data types, only python builtins.
-
-    Stores everything needed to replicate web app state, including the user plan and app config
-    """
-
-    def __init__(self, plan: dict):
-        self.plan = plan
-
-    @staticmethod
-    def from_dict(data: dict):
-        return State(data["plan"])
-
-    @staticmethod
-    def from_json_str(json_str: str):
-        return State.from_dict(json.loads(json_str))
-
-    @staticmethod
-    def from_b64_str(b64_str: str):
-        return State.from_json_str(json_from_base64(b64_str))
-
-    def to_dict(self) -> dict:
-        return {
-            "plan": self.plan,
-        }
-
-    def to_json_str(self) -> str:
-        # FIXME: json dump/parse/dump can be simplified to be faster
-        return minify_json(json.dumps(self.to_dict()))
-
-    def to_b64_str(self) -> str:
-        return base64_from_json(self.to_json_str())
-
-
 class PlanUploadForm(flask_wtf.FlaskForm):
     file_picker = flask_wtf.file.FileField(
         validators=[
@@ -101,22 +68,23 @@ class PlanUploadForm(flask_wtf.FlaskForm):
 
 
 class PlanTankSubform(wtforms.Form):
-    name = wtforms.fields.StringField(
+    # "name" field means something else
+    which = wtforms.fields.StringField(
         "Name",
         default="Primary",
-        validators = [
+        validators=[
             wtforms.validators.DataRequired(),
-        ]
+        ],
     )
+    # "type" field means something else
     kind = wtforms.fields.SelectField(
         "Type",
-        default = "AL80",
+        default="AL80",
         choices=[
-            (bungee.Tank(i).name, bungee.Tank(i).name)
-            for i in range(bungee.Tank.COUNT.value)
-        ]
+            (bungee.Tank(i).name, bungee.Tank(i).name) for i in range(bungee.Tank.COUNT.value)
+        ],
     )
-    mix_fO2 = wtforms.fields.FloatField(
+    fO2 = wtforms.fields.FloatField(
         "O2 Fraction",
         default=0.21,
         validators=[
@@ -126,29 +94,25 @@ class PlanTankSubform(wtforms.Form):
     )
     pressure = wtforms.fields.StringField(
         "Start Pressure",
-        default = "3000 psi",
+        default="3000 psi",
         validators=[
             # FIXME: custom validator to make sure that pint unit exists
             wtforms.validators.DataRequired()
         ],
     )
 
+
 class PlanProfileSubform(wtforms.Form):
     tank = wtforms.fields.StringField(
-        "Tank at Start",
-        default="Primary",
-        validators=[wtforms.validators.Optional()]
+        "Tank at Start", default="Primary", validators=[wtforms.validators.Optional()]
     )
     duration = wtforms.fields.StringField(
-        "Duration",
-        default="5 min",
-        validators=[wtforms.validators.DataRequired()]
+        "Duration", default="5 min", validators=[wtforms.validators.DataRequired()]
     )
     depth = wtforms.fields.StringField(
-        "Final Depth",
-        default="20 ft",
-        validators=[wtforms.validators.DataRequired()]
+        "Final Depth", default="20 ft", validators=[wtforms.validators.DataRequired()]
     )
+
 
 class PlanPlanForm(flask_wtf.FlaskForm):
     water = wtforms.fields.SelectField(
@@ -172,36 +136,101 @@ class PlanPlanForm(flask_wtf.FlaskForm):
             wtforms.validators.NumberRange(min=0.1, max=1.0),
         ],
     )
+    # FIXME: custom validator to make sure that pint unit exists
     scr_work = wtforms.fields.StringField(
         default="0.75 cuft / min",
-        validators=[
-            # FIXME: custom validator to make sure that pint unit exists
-            wtforms.validators.DataRequired()
-        ],
+        validators=[wtforms.validators.DataRequired()],
     )
+    # FIXME: custom validator to make sure that pint unit exists
     scr_deco = wtforms.fields.StringField(
         default="0.55 cuft /min",
-        validators=[
-            # FIXME: custom validator to make sure that pint unit exists
-            wtforms.validators.DataRequired()
-        ],
+        validators=[wtforms.validators.DataRequired()],
     )
     plot_button = wtforms.fields.SubmitField(label="Plot")
     save_button = wtforms.fields.SubmitField(label="Save")
-    tanks = wtforms.FieldList(
-        wtforms.FormField(PlanTankSubform),
-        min_entries=1,
-        max_entries=4
-    )
+    tanks = wtforms.FieldList(wtforms.FormField(PlanTankSubform), min_entries=1, max_entries=4)
     add_tank = wtforms.fields.SubmitField(label="Add Tank")
     remove_tank = wtforms.fields.SubmitField(label="Remove Tank")
     profile = wtforms.FieldList(
-        wtforms.FormField(PlanProfileSubform),
-        min_entries=1,
-        max_entries=20
+        wtforms.FormField(PlanProfileSubform), min_entries=1, max_entries=20
     )
     add_segment = wtforms.fields.SubmitField(label="Add Segment")
     remove_segment = wtforms.fields.SubmitField(label="Remove Segment")
+
+
+class State:
+    """No bungee/cenote data types, only python builtins.
+
+    Stores everything needed to replicate web app state, including the user plan and app config
+    """
+
+    def __init__(self, plan: dict):
+        self.plan = plan
+
+    @staticmethod
+    def from_dict(data: dict):
+        return State(data["plan"])
+
+    @staticmethod
+    def from_json_str(json_str: str):
+        return State.from_dict(json.loads(json_str))
+
+    @staticmethod
+    def from_b64_str(b64_str: str):
+        return State.from_json_str(json_from_base64(b64_str))
+
+    @staticmethod
+    def from_plan_plan_form(form: PlanPlanForm):
+        data = {
+            "plan": {},
+        }
+        # water
+        data["plan"]["water"] = form.water.data
+        # gf
+        data["plan"]["gf"] = {
+            "low": form.gf_lo.data,
+            "high": form.gf_hi.data,
+        }
+        # scr
+        data["plan"]["scr"] = {
+            "work": form.scr_work.data,
+            "deco": form.scr_deco.data,
+        }
+        # tanks
+        data["plan"]["tanks"] = {
+            tank.which.data: {
+                "type": tank.kind.data,
+                "pressure": tank.pressure.data,
+                "mix": {
+                    "fO2": tank.fO2.data,
+                },
+            }
+            for tank in form.tanks
+        }
+        # profile
+        data["plan"]["profile"] = [
+            {
+                "tank": segment.tank.data,
+                "duration": segment.duration.data,
+                "depth": segment.depth.data,
+            }
+            for segment in form.profile
+        ]
+        pprint.pprint(data)
+        return State.from_dict(data)
+
+    def to_dict(self) -> dict:
+        return {
+            "plan": self.plan,
+        }
+
+    def to_json_str(self) -> str:
+        # FIXME: json dump/parse/dump can be simplified to be faster
+        return minify_json(json.dumps(self.to_dict()))
+
+    def to_b64_str(self) -> str:
+        return base64_from_json(self.to_json_str())
+
 
 app = flask.Flask(__name__)
 
@@ -224,10 +253,11 @@ def plan(state_b64: str):
     }
 
     # parse url arguments
-    if state_b64 is not None:
-        state = State.from_b64_str(state_b64)
-    else:
-        state = None
+    # FIXME: store state in the form instead
+    # if state_b64 is not None:
+    #     state = State.from_b64_str(state_b64)
+    # else:
+    #     state = None
 
     # upload
     if upload_form.upload_button.data and upload_form.file_picker.data is not None:
@@ -263,8 +293,7 @@ def plan(state_b64: str):
     # plot
     # FIXME: validate_on_submit???
     if plan_form.plot_button.data:
-        if state is None:
-            raise Exception("no state populated")
+        state = State.from_plan_plan_form(plan_form)
         return flask.redirect(flask.url_for("plot", state_b64=state.to_b64_str()))
 
     # save
@@ -280,29 +309,34 @@ class PlotNavForm(flask_wtf.FlaskForm):
 
 @app.route("/plot/<state_b64>", methods=["POST", "GET"])
 def plot(state_b64: str):
+    print("here")
     # state must be well formed for this page to work at all
     state = State.from_b64_str(state_b64)
-
+    print("2")
     nav_form = PlotNavForm()
-
+    print("3")
     kwargs = {
         "nav_form": nav_form,
     }
-
+    print("4")
     if nav_form.plan_button.data:
         # go back with the current plan in the editor
         # FIXME: if editing functionality ever added, will need to send b64 from state, not
         # from the original arg
         return flask.redirect(flask.url_for("plan", state_b64=state_b64))
-
+    print("5")
     try:
+        print("6")
         input_plan = cenote.plan_from_dict(state.plan)
+        print("7")
         output_plan = bungee.replan(input_plan)
+        print("8")
         result = cenote.get_result(output_plan)
+        print("9")
     except Exception as exc:
         flask.flash("There's a problem with your dive plan:\n{}".format(traceback.format_exc()))
         return flask.render_template("plot.html", **kwargs)
-
+    
     plan_table_df = plots.get_plan_df(output_plan)
     kwargs["plan_table"] = pretty_html_table.build_table(
         plan_table_df,
@@ -311,7 +345,6 @@ def plot(state_b64: str):
         even_bg_color="#282828",
         even_color="white",
     )
-
     figs = [
         plots.get_depth_fig(result),
         plots.get_pressure_fig(result),
